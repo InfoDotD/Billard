721a9690c39b8d4e81a030d9fdc07c0fea381983
InfoDotD <134912396+InfoDotD@users.noreply.github.com>
2023-05-29 08:48:02 +0200

    Update Kugel5.java

--- Kugel5.java
+++ Kugel5.java
@@ -1,1 +1,47 @@
 
+import sum.kern.*;
+public class Kugel4
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    Buntstift hB;
+    
+    public Kugel4(int pH, int pV, int pWinkel, int pFarbe)
+    {
+        hB = new Buntstift();
+        hB.bewegeBis(pH,pV);
+        hB.dreheBis(0.0); 
+        hB.dreheBis(pWinkel);
+        hB.setzeFarbe(pFarbe);
+        this.zeichne(); 
+        
+    }
+
+    public void zeichne()
+    {
+        hB.zeichneKreis(15); 
+    }
+    
+    public void loesche()
+    
+   {
+       hB.radiere();
+       this.zeichne();
+       hB.normal(); 
+    }
+    
+    public void bewege()
+    {
+        this.loesche();
+        hB.bewegeUm(10);
+        this.zeichne(); 
+        this.pralleAb(); 
+    }
+    
+    public void pralleAb()
+    {
+        if(hB.hPosition() >= 780 || hB.hPosition() <= 20 || hB.vPosition() <= 20 || hB.vPosition() >= 580)
+        {
+            hB.dreheUm(180);
+        }       
+    }
+}
