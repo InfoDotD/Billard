5d5eda08d62b44bbfb342874bb5c6ab7a4d8960a
InfoDotD <134912396+InfoDotD@users.noreply.github.com>
2023-05-29 08:35:10 +0200

    Update Steuerprogramm_Billard.java

--- Steuerprogramm_Billard.java
+++ Steuerprogramm_Billard.java
@@ -13,6 +13,7 @@ public class Steuerprogramm_Billard
     private Kugel3 hK31;
     
     private Kugel4 hK4;
+    private Kugel5 hK5;
 
     /**
      * Constructor for objects of class Steuerprogramm
@@ -26,6 +27,7 @@ public class Steuerprogramm_Billard
         hK31 = new Kugel3(200, 300);
         
         hK4 = new Kugel4(400, 400, 80, 7);
+        hK5 = new Kugel4(300, 300, 50, 9);
         this.spiele();
     }
 
